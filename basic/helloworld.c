d562d1eb3dab32eb39c7156affe7c36f1f1072dc
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-05 08:40:55 +0100

    hello world

---
+++ basic/helloworld.c
@@ -0,0 +1,7 @@
+echo "# C-ver-fundamental" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HungTruong4689/C-ver-fundamental.git
+git push -u origin main
